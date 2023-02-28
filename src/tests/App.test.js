4144f0ed2b8aaad4e853243d820f56cb02920ead
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-28 16:28:23 -0300

    created tests for App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('testing App.js component', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testing App.js component', () => {
+  it('test if all 3 links appears on screen', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favorite = screen.getByText('Favorite Pokémon');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+});
