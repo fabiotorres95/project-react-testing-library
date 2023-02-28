3603955473a53dde188f69241c8fc5199d4e359e
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-28 17:29:04 -0300

    created tests for NotFound component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('tests NotFound component', () => {
+  it('tests if message and image appear on screen', () => {
+    renderWithRouter(<NotFound />);
+
+    const message = screen.getByText('Page requested not found');
+    const image = screen.getByRole('img');
+
+    expect(message).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
