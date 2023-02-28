1293d4fa214c4a2580fb672a24d95fde4c4bc185
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-28 17:05:27 -0300

    created tests for FavoritePokemon component

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Tests for FavoritePokemon component', () => {
+  it('shows message when there is no favorite pokémons', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const message = screen.getByText('No favorite Pokémon found');
+
+    expect(message).toBeInTheDocument();
+  });
+
+  it('tests if only the favorite pokémons appear on screen', () => {
+    renderWithRouter(<App />);
+
+    const details = screen.getByText('More details');
+    userEvent.click(details);
+
+    const favoriteLink = screen.getByText('Favorite Pokémon');
+    const favoriteBox = screen.getByText('Pokémon favoritado?');
+
+    userEvent.click(favoriteBox);
+    userEvent.click(favoriteLink);
+
+    const pokemon = screen.getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
+  });
+});
