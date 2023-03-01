fb9a49b8e7f081d9282ed5522a182cb9dc1ee18f
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-28 21:11:21 -0300

    pokemonDetails has a bit more coverage, but not done

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,12 +1,15 @@
 import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import PokemonDetails from '../pages/PokemonDetails';
 import { isPokemonFavoriteByIdType } from '../types';
 import pokemonList from '../data';
 import renderWithRouter from '../renderWithRouter';
 
+// const app = require('../App');
+
 describe('tests for PokemonDetails component', () => {
   it('check if all the details that appear on screen match with the pokemon selected', () => {
-    const onUpdateFavoritePokemon = jest.fn().mockReturnValue('not favorite');
+    const onUpdateFavoritePokemon = jest.fn();
 
     renderWithRouter(<PokemonDetails
       isPokemonFavoriteById={ isPokemonFavoriteByIdType }
@@ -20,6 +23,8 @@ describe('tests for PokemonDetails component', () => {
     const info = screen.getByText('This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.');
     const location = screen.getByText('Game Locations of Pikachu');
     const images = screen.getAllByRole('img');
+    const forest = screen.getByText('Kanto Viridian Forest');
+    const plant = screen.getByText('Kanto Power Plant');
     const label = screen.getByText('Pokémon favoritado?');
 
     expect(title).toBeInTheDocument();
@@ -28,8 +33,38 @@ describe('tests for PokemonDetails component', () => {
     expect(location).toBeInTheDocument();
     expect(images[1]).toBeInTheDocument();
     expect(images[1]).toHaveProperty('src', 'https://archives.bulbagarden.net/media/upload/0/08/Kanto_Route_2_Map.png');
+    expect(images[1]).toHaveProperty('alt', 'Pikachu location');
+    expect(forest).toBeInTheDocument();
     expect(images[2]).toBeInTheDocument();
     expect(images[2]).toHaveProperty('src', 'https://archives.bulbagarden.net/media/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(images[2]).toHaveProperty('alt', 'Pikachu location');
+    expect(plant).toBeInTheDocument();
     expect(label).toBeInTheDocument();
   });
+
+  // it('check if favorite box shows a star on screen', () => {
+  //   const app = require('../App');
+
+  //   const mockFunction = jest.fn();
+  //   const onUpdateFavoritePokemon = jest.fn()
+  //     .mockReturnValue(render(<img
+  //       className="favorite-icon"
+  //       src="/star-icon.svg"
+  //       alt="Pikachu is marked as favorite"
+  //     />));
+
+  //   renderWithRouter(<PokemonDetails
+  //     isPokemonFavoriteById={ isPokemonFavoriteByIdType }
+  //     match={ { params: { id: '25' } } }
+  //     onUpdateFavoritePokemon={ mockFunction }
+  //     pokemonList={ pokemonList }
+  //   />);
+
+  //   const label = screen.getByText('Pokémon favoritado?');
+
+  //   userEvent.click(label);
+
+  //   const star = screen.getByAltText('Pikachu is marked as favorite');
+  //   expect(star).toBeInTheDocument();
+  // });
 });
